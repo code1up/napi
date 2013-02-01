a0f63b8985770ed51388639554f6b04bd8f03920
Alan Gorton <dev.code1up@googlemail.com>
2013-02-01 09:56:55 +0000

    wip

---
+++ server.js
@@ -0,0 +1,25 @@
+var util = require("util");
+var request = require("request");
+
+// Express.
+var express = require("express");
+
+// Create Express app.
+var app = express();
+
+app.use(express.bodyParser());
+app.use(app.router);
+
+app.get("/", function(request, response) {
+	response.header("content-type", "application/json");
+
+	response.send(200, {
+		status: "OK"
+	});
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+	console.log(util.format("Listening on %d.", port));
+});
